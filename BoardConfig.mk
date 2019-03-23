1099ee87403a48caea7fa06236ee40dee6ce74c1
ppooopppooo <ppooopppooo@github.com>
2019-03-23 14:09:19 +0800

    set display densit to 428

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,7 +30,7 @@ TARGET_OTA_ASSERT_DEVICE := sagit
 
 # LCD Density
 PRODUCT_GENERIC_PROPERTIES += \
-    ro.sf.lcd_density=480
+    ro.sf.lcd_density=428
 
 # SELinux
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
