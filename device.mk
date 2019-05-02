e0df4a4a5940adb45ebd5d77b3d12366942cc7a6
ppooopppooo <46632649+ppooopppooo@users.noreply.github.com>
2019-05-03 01:20:18 +0800

    sagit：remove gcam

--- device.mk
+++ device.mk
@@ -57,8 +57,5 @@ PRODUCT_PACKAGES += \
 # Miui Camera
 $(call inherit-product, vendor/XiaomiCustom/MiuiCamera/MiuiCamera.mk)
 
-# Google Camera
-$(call inherit-product, vendor/XiaomiCustom/GoogleCamera/GoogleCamera.mk)
-
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/sagit/sagit-vendor.mk)
