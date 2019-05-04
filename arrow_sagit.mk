e4bb8d84a42709b45e60b58b464fe0b0b0ed4841
ppooopppooo <46632649+ppooopppooo@users.noreply.github.com>
2019-05-04 18:23:56 +0800

    syberia

--- arrow_sagit.mk
+++ arrow_sagit.mk
@@ -19,12 +19,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common AospExtended stuff.
-<<<<<<< HEAD:arrow_sagit.mk
-$(call inherit-product, vendor/arrow/config/common.mk)
-=======
 $(call inherit-product, vendor/syberia/config/common_full_phone.mk)
->>>>>>> parent of 3bbf24a... Update and rename syberia_sagit.mk to bootleg_sagit.mk:syberia_sagit.mk
+
 
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1080
@@ -34,11 +30,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 # Inherit from sagit device
 $(call inherit-product, device/xiaomi/sagit/device.mk)
 
-<<<<<<< HEAD:arrow_sagit.mk
-PRODUCT_NAME := arrow_sagit
-=======
 PRODUCT_NAME := syberia_sagit
->>>>>>> parent of 3bbf24a... Update and rename syberia_sagit.mk to bootleg_sagit.mk:syberia_sagit.mk
 PRODUCT_DEVICE := sagit
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 6
