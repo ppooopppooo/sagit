c98449541faf95901683ea1f5e8b83d1be770d29
ppooopppooo <46632649+ppooopppooo@users.noreply.github.com>
2019-05-14 12:43:42 +0800

    Update syberia_sagit.mk

--- syberia_sagit.mk
+++ syberia_sagit.mk
@@ -19,8 +19,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common AospExtended 
-$(call inherit-product, vendor/syberia/config/common_full_phone.mk)
+# Inherit some common Syberia
+$(call inherit-product, vendor/syberia/config/common.mk)
 
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1080
